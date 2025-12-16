2aa97412abd22abbd515bf2e57392dbaee7f7c1d
billelkhanfr <khanfri.billel@yahoo.fr>
2025-12-16 14:32:36 +0100

    rename Navbar to solve built issue

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Navbar from "./components/Navbar.js";
+import Navbar from "./components/Navigation.js";
 import Footer from "./components/Footer.js";
 
 export const metadata = {
